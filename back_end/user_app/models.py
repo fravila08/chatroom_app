76ae8827b8b9b09025efc96ad207ed729ab5ba6f
fravila08 <fr4v1l4@gmail.com>
2024-08-23 21:44:30 -0700

    implement user authentication api endpoints

--- back_end/user_app/models.py
+++ back_end/user_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
+from rest_framework.serializers import ModelSerializer
+from rest_framework.authtoken.models import Token
 
 
 # Create your models here.
@@ -7,3 +10,22 @@ class AppUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = AppUser
+        fields = "__all__"
+
+    def create(self, validated_data):
+        try:
+            password = validated_data.pop('password')  
+            new_user = AppUser(**validated_data)  
+            new_user.set_password(password)
+            new_user.clean()
+            new_user.save()
+            Token.objects.create(user=new_user)
+            return new_user
+        except Exception as e:
+            return e
--- back_end/user_app/views.py
+++ back_end/user_app/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status as s
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login, logout
+from .models import UserSerializer
 
 # Create your views here.
+
+
+class Register(APIView):
+    def post(self, request):
+        data = request.data.copy()
+        data["username"] = request.data.get("email")
+        new_user = UserSerializer(data=data, partial=True)
+        if new_user.is_valid():
+            new_user = new_user.save()
+            login(request, new_user)
+            return Response(
+                {"username": new_user.email, "token": new_user.auth_token.key},
+                status=s.HTTP_201_CREATED,
+            )
+        else:
+            return Response(new_user.errors, status=s.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        data = request.data.copy()
+        email =  data.get("email")
+        password = data.get("password")
+        user = authenticate(request, username=email, password=password)
+        if user:
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(
+                {"username": user.email, "token": token.key},
+                status=s.HTTP_201_CREATED,
+            )
+        else:
+            return Response("No user matching credentials", status=s.HTTP_404_NOT_FOUND)
+
+
+class UserViews(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class Info(UserViews):
+    def get(self, request):
+        user = request.user
+        return Response(
+            {"username": user.email, "token": user.auth_token.key},
+            status=s.HTTP_200_OK,
+        )
+
+
+class LogOut(UserViews):
+    def post(self, request):
+        user = request.user
+        logout(request)
+        user.aut_token.delete()
+        return Response("Logged Out", status=s.HTTP_200_OK)
